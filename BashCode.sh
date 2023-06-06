8327dd4a9c686194d85f02979b4a17fe942840a7
valeriojer <134316997+valeriojer@users.noreply.github.com>
2023-06-06 08:53:18 -0400

    Update BashCode.sh

--- BashCode.sh
+++ BashCode.sh
@@ -36,58 +36,58 @@ Use only the find and cp commands. You will need to utilize the -exec option on 
 
 find ~/1123 -name *.txt ! -name *~* -exec cp {} ~/CUT \;
 ---------------------------------------------------------------------------
-Activity: Using ONLY the find command, find all empty files/directories in directory /var and print out ONLY the filename (not absolute path),
+3 Activity: Using ONLY the find command, find all empty files/directories in directory /var and print out ONLY the filename (not absolute path),
 and the inode number, separated by newlines.
 
 find /var -empty -printf "%i %f\n"
 ----------------------------------------------------------------------------
-Activity: Using ONLY the find command, find all files on the system with inode 4026532575 and print only the filename to the screen, not the absolute path to the file,
+4 Activity: Using ONLY the find command, find all files on the system with inode 4026532575 and print only the filename to the screen, not the absolute path to the file,
 separating each filename with a newline. Ensure unneeded output is not visible.
 
 find / -inum 4026532575 -printf "%f\n"
 -----------------------------------------------------------------------------
-Activity: Using only the ls -l and cut Commands, write a BASH script that shows all filenames with extensions ie: 1.txt, etc., but no directories, in $HOME/CUT.
+5 Activity: Using only the ls -l and cut Commands, write a BASH script that shows all filenames with extensions ie: 1.txt, etc., but no directories, in $HOME/CUT.
 Write those to a text file called names in $HOME/CUT directory. Omit the names filename from your output.
 
 ls -l ~/CUT | cut -d. -s -f1,2 | cut -d':' -f2| cut -d'' -f2 > ~/CUT/names
 ls -l /*.* | cut -d'/' -f3
 ls -l ~/CUT | cut -d. -s -f1,2 | rev | cut -d" " -f1 | rev 
 -------------------------------------------------------------------------------
-Activity: Write a basic bash script that greps ONLY the IP addresses in the text file provided (named StoryHiddenIPs in the current directory);
+6 Activity: Write a basic bash script that greps ONLY the IP addresses in the text file provided (named StoryHiddenIPs in the current directory);
 sort them uniquely by number of times they appear.
 
 egrep '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -o StoryHiddenIPs | sort | uniq -c | sort -r | awk {print}
 -------------------------------------------------------------------------------
-Activity: Using ONLY the awk command, write a BASH one-liner script that extracts ONLY the names of all the system and user accounts that are not UIDs 0-3.
+7 Activity: Using ONLY the awk command, write a BASH one-liner script that extracts ONLY the names of all the system and user accounts that are not UIDs 0-3.
 Only display those that use /bin/bash as their default shell. the input file is named $HOME/passwd and is located in the current directory.
 Output the results to a file called $HOME/SED/names.txt
 
 awk -F: '($3 > 3 && $NF == "/bin/bash") {print $1}' ~/passwd > ~/SED/names.txt
 -------------------------------------------------------------------------------
-Activity: Find all dmesg kernel messages that contain CPU or BIOS (uppercase) in the string, but not usable or reserved (case-insensitive)  
+8 Activity: Find all dmesg kernel messages that contain CPU or BIOS (uppercase) in the string, but not usable or reserved (case-insensitive)  
 Print only the msg itself, omitting the bracketed numerical expressions ie: [1.132775]
 
 dmesg | egrep "(CPU)|(BIOS)" | egrep -v "(usable)|(reserved)" | cut -d] -f2-
 --------------------------------------------------------------------------------
-Activity: Write a Bash script using "Command Substitution" to replace all passwords, using openssl, from the file $HOME/PASS/shadow.txt with the MD5 encrypted password: Password1234, with salt: bad4u
+9 Activity: Write a Bash script using "Command Substitution" to replace all passwords, using openssl, from the file $HOME/PASS/shadow.txt with the MD5 encrypted password: Password1234, with salt: bad4u
 Output of this command should go to the screen/standard output. You are not limited to a particular command, however you must use openssl. 
 Type man openssl passwd for more information.
 
 A=$(openssl passwd -1 -salt bad4u Password1234)
 awk -F: -v "awk_var=$A" 'BEGIN {OFS=":"} {$2=awk_var} {print $0}' ~/PASS/shadow.txt
 ----------------------------------------------------------------------------------
-Activity: Using ONLY sed, write all lines from $HOME/passwd into $HOME/PASS/passwd.txt that do not end with either /bin/sh or /bin/false.
+10 Activity: Using ONLY sed, write all lines from $HOME/passwd into $HOME/PASS/passwd.txt that do not end with either /bin/sh or /bin/false.
 
 sed '/\/bin\/sh/d' ~/passwd | sed '/false/d' > ~/PASS/passwd.txt
 ----------------------------------------------------------------------------------
-Activity: Using find, find all files under the $HOME directory with a .bin extension ONLY.
+11 Activity: Using find, find all files under the $HOME directory with a .bin extension ONLY.
 Once the file(s) and their path(s) have been found, remove the file name from the absolute path output.
 Ensure there is no trailing / at the end of the directory path when outputting to standard output. You may need to sort the output depending on the command(s) you use.
 
 find ~/ -name "*.bin" | rev | cut -d/ -f2- | rev | sort | uniq
 find ~/ -name "*.bin" | awk 'BEGIN{FS="/";OFS="/"}{NF=NF-1}{print $0}' | sort -u
 -----------------------------------------------------------------------------------
-Activity: Write a script which will copy the last entry/line in the passwd-like file specified by the $1 positional parameter
+12 Activity: Write a script which will copy the last entry/line in the passwd-like file specified by the $1 positional parameter
 Modify the copied line to change: User name to the value specified by $2 positional parameter
 Used id and group id to the value specified by $3 positional parameter Home directory to a directory matching the user name specified by $2 positional parameter under the /home directory
 The default shell to 'bin/bash' Append the modified line to the end of the file
@@ -105,7 +105,7 @@ modline=$(echo "$lastline" | awk -F ':' -v var="$uname" -v fid=$uid -v sid=$gid 
 
 echo "$modline" >> "$path"
 -------------------------------------------------------------------------------------
-Activity: Find all executable files under the following four directories:
+13 Activity: Find all executable files under the following four directories:
         /bin
         /sbin
         /usr/bin
@@ -115,12 +115,12 @@ Sort the filenames with absolute path, and get the md5sum of the 10th file from 
 a=$(find /bin /sbin /usr/bin /usr/sbin -type f -executable | sort | head | tail -1) 
 md5sum $a | awk -F' ' '{print $1}'
 --------------------------------------------------------------------------------------
-Activity: sing any BASH command complete the following: ort the /etc/passwd file numerically by the GID field.
+14 Activity: sing any BASH command complete the following: ort the /etc/passwd file numerically by the GID field.
 For the 10th entry in the sorted passwd file, get an md5 hash of that entry’s home directory. utput ONLY the MD5 hash of the directorys name to standard output.
 
 awk -F: '{print $4,$6}' /etc/passwd | sort -n | head | tail -1 | awk -F " " '{print $2}'| md5sum | awk -F " " '{print $1}' 
 --------------------------------------------------------------------------------------
-Activity: Write a script which will find and hash the contents 3 levels deep from each of these directories: /bin /etc /var
+15 Activity: Write a script which will find and hash the contents 3 levels deep from each of these directories: /bin /etc /var
 Your script 
     
 a=$(find /bin /etc /var -maxdepth 3) 
@@ -131,7 +131,7 @@ c=$(cat out.txt | wc -l)
 echo Successfully Hashed Files: $c
 echo Unsuccessfully Hashed Directories: $b    
 --------------------------------------------------------------------------------------
-Activity: Design a script that detects the existence of directory: $HOME/.ssh
+16 Activity: Design a script that detects the existence of directory: $HOME/.ssh
 Upon successful detection, copies any and all files from within the directory $HOME/.ssh to directory $HOME/SSH and produce no output. You will need to create $HOME/SSH.
 Upon un-successful detection, displays the error message "Run ssh-keygen" to the user.
 
@@ -177,8 +177,7 @@ echo '12345' | md5sum | cut -d" " -f1 > $HOME/ZIP/file1
 echo '6789' | md5sum | cut -d" " -f1 > $HOME/ZIP/file2
 echo 'abcdef' | md5sum | cut -d" " -f1 > $HOME/ZIP/file3
 zip $HOME/ZIP/file.zip $HOME/ZIP/* -jq
-cd $HOME/ZIP/
-tar -cvzf $HOME/ZIP/file.tar.gz file.zip
+tar -czf $HOME/ZIP/file.tar.gz -C $HOME/ZIP/ file.zip
 ---------------------------------------------------------------------------------------------------------
 19 Activity: Design a basic FOR Loop that iteratively alters the file system and user entries in a passwd-like file for new users: LARRY, CURLY, and MOE.
 Each user should have an entry in the $HOME/passwd file
@@ -211,12 +210,11 @@ do
     fi
 done
 ----------------------------------------------------------------------------------------------
-Activity:
-
-    Write a bash script that will find all the files in the /etc directory, and obtains the octal permission of those files. The stat command will be useful for this task.
-    Depending how you go about your script, you may find writing to the local directory useful. What you must seperate out from the initial results are the octal permissions of 0-640 and those equal to and greater than 642, ie 0-640 goes to low.txt, while 642 is sent to high.txt.
-    Have your script uniquely sort the contents of the two files by count, numerically-reversed, and output the results, with applicable titles, to the screen. An example of the desired output is shown below.
-        NOTE: There is a blank line being printed between the two sections of the output below.
+20 Activity: Write a bash script that will find all the files in the /etc directory, and obtains the octal permission of those files. 
+The stat command will be useful for this task. Depending how you go about your script, you may find writing to the local directory useful.
+What you must seperate out from the initial results are the octal permissions of 0-640 and those equal to and greater than 642, ie 0-640 goes to low.txt,
+while 642 is sent to high.txt. Have your script uniquely sort the contents of the two files by count, numerically-reversed, and output the results, 
+with applicable titles, to the screen. An example of the desired output is shown below. NOTE: There is a blank line being printed between the two sections of the output below.
         
 #!/bin/bash
   A=$(find /etc -type f)
