37de630a3c588c33b1b5bf4f6f7325c3296325ab
valeriojer <134316997+valeriojer@users.noreply.github.com>
2023-06-08 08:33:39 -0400

    Update PsCode.ps1

--- PsCode.ps1
+++ PsCode.ps1
@@ -55,12 +55,14 @@ $quo = $var1 / $var2
 _______________________________________________________________________________________________________
 #####Practical Exercise: The Pipeline
     #Display the start time of the earliest and latest running processes
-  ps | Sort-Object -Property starttime -ErrorAction SilentlyContinue| select -Skip 1 -first 1 -Last 1  | ft name, starttime
+ps | Sort-Object -Property starttime -ErrorAction SilentlyContinue| select -Skip 1 -first 1 -Last 1  | ft name, starttime
     #Identify a cmdlet that returns the current date and time then using this cmdlet and Select-object, display only the current day of the week
-  date | Select-object -Expandproperty DayOfWeek   
+date | Select-object -Expandproperty DayOfWeek   
     #Extend the expression to sort the list by install date, and display only the install date and hotfix ID.
-  Get-HotFix | Sort-Object -Property Installedon | ft InstalledOn, hotfixid   
+Get-HotFix | Sort-Object -Property Installedon | ft InstalledOn, hotfixid   
     #Extend the expression further, but this time sort by description, include description, hotfix ID, and install Date.
-  Get-HotFix | Sort-Object -Property Description | ft Description, installedon,hotfixid
-  
+Get-HotFix | Sort-Object -Property Description | ft Description, installedon,hotfixid
+_______________________________________________________________________________________________________
+#####Practical Exercise: Reverse Arrays
+    #Create an array containing a range with a random starting and stopping point. The starting point will be a random number from -10 through 0. The ending point will be a random number from 1 through 20.
    
